0467cd1a9407005eb310b093772eb1e2ffdc9a91
ginnnnnn <aginlo@hotmail.com>
2019-05-17 18:58:24 +0800

    code-splitting

---
+++ src/components/Page3.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Page1 = ({ onRouteChange }) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Welcome to React</code>
+        </p>
+        <button onClick={() => onRouteChange("page1")}>Page1</button>
+        <button onClick={() => onRouteChange("page2")}>Page2</button>
+        <button className="disabled">Page3</button>
+      </header>
+    </div>
+  );
+};
+
+export default Page1;
